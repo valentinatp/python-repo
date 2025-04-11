70ac9060b6fc88847164becea7bb32aaa128298c
Valentina Toledo <valentina.toledoporma@gmail.com>
2025-04-11 00:19:15 -0400

    Se agrega funcionalidad para consumo de api y captura de codigos

--- consumo-api/api-server-requests.py
+++ consumo-api/api-server-requests.py
@@ -1,12 +1,16 @@
 import requests
 def consultar_api_fetch(endpoint):
-    url_consultada = "https://ftx.us/api"
-    response = requests.get(url_consultada + endpoint)
-    print(response)
-    # for i in response:
-    #     print(i)
-    # if response[200] == "200":
-    #     print("La solicitud ha tenido exito.")
+    url_consultada = "https://jsonplaceholder.typicode.com/"
+    response = requests.get(url_consultada + endpoint);
+    print(response);
+    if response.status_code == 200:
+        print("-- La solicitud ha tenido exito --")
+    elif response.status_code == 300:
+        print("-- Redirección en proceso --")
+    elif response.status_code == 404:
+        print("-- Solicitud incorrecta --")
+    elif response.status_code == 500:
+        print("-- Error de conexión con el Servidor --")
     return
 endpoint_ingresado = input("Ingresa el endpoint para consultar: ")
 print(f"Consultando Endopoint ingresado... ... ...")
